2ecc1c65633bf553ca79d7eb4a4541bb93edb8e9
apoczekalewicz <Artur@Poczekalewicz.net>
2022-04-16 16:05:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,22 @@
-        <?php
-        //whether ip is from share internet
-        if (!empty($_SERVER['HTTP_CLIENT_IP']))
-          {
-            $ip_address = $_SERVER['HTTP_CLIENT_IP'];
-            echo "HTTP_CLIENT_IP: $ip_address<br>";
-          }
-        //whether ip is from proxy
-        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-          {
-            $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
-            echo "HTTP_X_FORWARDED_FOR: $ip_address<br>";
-          }
-        //whether ip is from remote address
-        else
-          {
-            $ip_address = $_SERVER['REMOTE_ADDR'];
-            echo "REMOTE_ADDR: $ip_address<br>";
-          }
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php 
 
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) { $http_client_ip = $_SERVER['HTTP_CLIENT_IP']; }
+if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) { $http_x_forwarded_for = $_SERVER['HTTP_X_FORWARDED_FOR']; }
+if (!empty($_SERVER['REMOTE_ADDR'])) { $remote_addr = $_SERVER['REMOTE_ADDR']; }
 $localIP = getHostByName(getHostName());
-echo "getHostByName(): $localIP<br>";
 $localHostName = getHostName();
-echo "getHostName(): $localHostName<br>";
-?>
 
+echo "HTTP_CLIENT_IP: $http_client_ip<br>";
+echo "HTTP_X_FORWARDED_FOR: $http_x_forwarded_for<br>";
+echo "REMOTE_ADDR: $remote_addr<br>";
+echo "getHostByName(): $localIP<br>";
+echo "getHostName(): $localHostName<br>";
+?> 
+ </body>
+</html>
